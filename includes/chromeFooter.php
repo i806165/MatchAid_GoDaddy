a3722c437e64468f876ac3e16ffcf552b1268d6d
i806165 <cdisney99@gmail.com>
2026-02-23 10:14:49 -0500

    Changed summary icon

--- includes/chromeFooter.php
+++ includes/chromeFooter.php
@@ -44,7 +44,9 @@
     </button>
 
     <button type="button" class="maNavBtn" data-nav="summary" aria-label="Summary">
-      <span class="maNavIcon" aria-hidden="true">🗏</span>
+      <span class="maNavIcon" aria-hidden="true">
+        <svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"/></svg>
+      </span>
       <span class="maNavLabel">Summary</span>
     </button>
     
