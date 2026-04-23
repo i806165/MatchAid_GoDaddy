03879a94a47a7307c9e7f7c94d0045fb3c1abf39
i806165 <cdisney99@gmail.com>
2026-04-23 17:13:14 -0400

    Fix wizard step-3

--- app/game_settings/gamesettings_view.php
+++ app/game_settings/gamesettings_view.php
@@ -124,7 +124,7 @@
             <div class="gsWizFieldLabel">Scoring Method</div>
             <div class="wizChips" id="gsWizMethodChips">
               <button class="wizChip" data-val="NET"       onclick="window.gsWiz.selectMethod('NET')">NET</button>
-              <button class="wizChip" data-val="ADJ GROSS" onclick="window.gsWiz.selectMethod('ADJ GROSS')">ADJ GROSS</button>
+              <button class="wizChip" data-val="GROSS" onclick="window.gsWiz.selectMethod('ADJ GROSS')">ADJ GROSS</button>
             </div>
           </div>
 
