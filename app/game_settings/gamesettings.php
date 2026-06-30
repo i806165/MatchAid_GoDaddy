4fc1c6c59542fd8604774d9c5f83a8ef19f38b0a
i806165 <cdisney99@gmail.com>
2026-06-29 20:47:18 -0400

    Cache busting logic

--- app/game_settings/gamesettings.php
+++ app/game_settings/gamesettings.php
@@ -98,8 +98,8 @@ $pageHelpKey = ServicePageHelp::keyFromControllerFile(__FILE__);
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
 
-  <link rel="stylesheet" href="/assets/css/ma_shared.css">
-  <link rel="stylesheet" href="/assets/css/game_settings.css?v=1">
+  <link rel="stylesheet" href="<?= ma_asset('/assets/css/ma_shared.css') ?>">
+  <link rel="stylesheet" href="<?= ma_asset('/assets/css/game_settings.css') ?>">
 </head>
 <body>
   <?php include __DIR__ . "/../../includes/chromeHeader.php"; ?>
@@ -129,10 +129,10 @@ $pageHelpKey = ServicePageHelp::keyFromControllerFile(__FILE__);
     };
   </script>
 
-  <script src="/assets/js/ma_shared.js"></script>
-  <script src="/assets/modules/recalculate_handicaps.js"></script>
-  <script src="/assets/modules/addCalendar.js"></script>
-  <script src="/assets/modules/pageHelp.js?v=1"></script>
-  <script src="/assets/pages/game_settings.js"></script>
+  <script src="<?= ma_asset('/assets/js/ma_shared.js') ?>"></script>
+  <script src="<?= ma_asset('/assets/modules/recalculate_handicaps.js') ?>"></script>
+  <script src="<?= ma_asset('/assets/modules/addCalendar.js') ?>"></script>
+  <script src="<?= ma_asset('/assets/modules/pageHelp.js') ?>"></script>
+  <script src="<?= ma_asset('/assets/pages/game_settings.js') ?>"></script>
 </body>
 </html>
