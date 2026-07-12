a7f4bc776d00d1e3b155c0b10e10a203e9907546
i806165 <cdisney99@gmail.com>
2026-07-12 16:18:34 -0400

    Fix bug

--- app/game_players/gameplayers.php
+++ app/game_players/gameplayers.php
@@ -145,7 +145,7 @@ $pageHelpKey = ServicePageHelp::keyFromControllerFile(__FILE__);
   <script src="<?= ma_asset('/assets/modules/module_sourceNonRated.js') ?>"></script>
   <script src="<?= ma_asset('/assets/modules/module_sourceEventRoster.js') ?>"></script>
   <script src="<?= ma_asset('/assets/modules/actions_menu.js') ?>"></script>
-  <script src="<?= ma_asset('/assets/modules/manage_teams.js') ?>"></script>
+  <script src="<?= ma_asset('/assets/modules/module_defineTeams.js') ?>"></script>
   <script src="<?= ma_asset('/assets/modules/module_defineFlights.js') ?>"></script>
   <script src="<?= ma_asset('/assets/modules/module_defineHandicapSettings.js') ?>"></script>
   <script src="<?= ma_asset('/assets/modules/recalculate_handicaps.js') ?>"></script>
