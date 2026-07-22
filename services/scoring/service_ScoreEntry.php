447408eddf41065855f8e64a99dbaef0f7236d4c
i806165 <cdisney99@gmail.com>
2026-07-22 18:36:19 -0400

    missing require once

--- services/scoring/service_ScoreEntry.php
+++ services/scoring/service_ScoreEntry.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require_once __DIR__ . "/../../bootstrap.php";
 require_once __DIR__ . '/service_ScoreCard.php';
 require_once MA_SVC_DB . '/service_dbPlayers.php';
+require_once MA_SVC_DB . '/service_dbGames.php';
 require_once __DIR__ . '/service_ScoreRotation.php';
 
 /**
