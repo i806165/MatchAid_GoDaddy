7498a0b598c9ff98aa1b74f57e1a949ecffa2bac
i806165 <cdisney99@gmail.com>
2026-09-22 21:25:37 -0400

    UI Adjustments

--- app/score_gis/scoregis_view.php
+++ app/score_gis/scoregis_view.php
@@ -4,10 +4,6 @@
 
 <div class="maCards">
     <section class="maCard" aria-label="Course GPS">
-        <div class="maCard__hdr">
-            <div class="maCard__title">COURSE GPS</div>
-        </div>
-
         <div class="maCard__body">
             <div id="scoreGisModuleHost"></div>
         </div>
